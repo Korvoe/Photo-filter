26775ebea40eb8054fd979fea8e7b0e52f479740
Korvoe <36694021+Korvoe@users.noreply.github.com>
2018-12-08 01:13:13 +0900

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -19,17 +19,24 @@ class Filter(tkinter.Frame):
         self.show_menu()
         self.master.config(menu = self.menubar)
 
+        #Initialization of size and self.img
         self.img = 0
+        self.image_frame_width = screen_width / 1.5
+        self.image_frame_height = screen_height
+        self.frame_for_configurations_width = screen_width / 6
+        self.frame_for_configurations_height = screen_height
+        self.frame_for_filters_width = screen_width / 6
+        self.frame_for_filters_height = screen_height
         
         ##Frame for something else(No idea for what, but may be useful)
         self.frame_for_configurations = tkinter.Frame(self.master, bd = 3, relief = tkinter.SUNKEN,
-                                               width = screen_width / 6, height = screen_height)
-        self.frame_for_configurations.pack(side = tkinter.RIGHT)
+                                               width = self.frame_for_configurations_width, height = self.frame_for_configurations_height)
+        self.frame_for_configurations.pack(sid = tkinter.RIGHT)
+        self.frame_for_configurations.pack_propagate(0)
 
         ##Setting a frame and canvas inside of a main frame(master), at which the image will be shown.
         #######################################################################################################
-        self.image_frame_width = screen_width / 1.5
-        self.image_frame_height = screen_height
+        
 
         #Frame
         self.image_frame = tkinter.Frame(self.master, relief = tkinter.SUNKEN,
@@ -44,9 +51,9 @@ class Filter(tkinter.Frame):
 
         ##Frame for filters, as you may notice from the variable`s name
         self.frame_for_filters = tkinter.Frame(self.master, bd = 3, relief = tkinter.SUNKEN,
-                                     width = screen_width / 6, height = screen_height)
-        self.frame_for_filters.pack(side = tkinter.RIGHT)
-        
+                                     width = self.frame_for_filters_width, height = self.frame_for_filters_height)
+        self.frame_for_filters.pack(side = tkinter.RIGHT, expand = 1)
+        self.frame_for_filters.pack_propagate(0)
     
     def show_menu(self):
         self.menubar = tkinter.Menu(self.master)
@@ -65,12 +72,61 @@ class Filter(tkinter.Frame):
 
         ##Creating radiobutton for each filter.
         tkinter.Radiobutton(self.frame_for_filters, text = "Original", variable = self.v, value = 1,
-                            command = self.original, pady = 5).pack(anchor = tkinter.W)
+                            command = self.original, pady = 5).pack(anchor = tkinter.N)
         tkinter.Radiobutton(self.frame_for_filters, text = "Black and White", variable = self.v, value = 2,
-                            command = self.black_and_white, pady = 5).pack(anchor = tkinter.W)
+                            command = self.black_and_white, pady = 5).pack(anchor = tkinter.N)
         tkinter.Radiobutton(self.frame_for_filters, text = "Negative", variable = self.v, value = 3,
-                            command = self.negative, pady = 5).pack(anchor = tkinter.W)
+                            command = self.negative, pady = 5).pack(anchor = tkinter.N)
+
+    def image_configurations(self):
+        brightness_var = 0
+        brightness_frame = tkinter.Frame(self.frame_for_configurations, bd = 3, relief = tkinter.SUNKEN)
+        brightness_frame.pack(side = tkinter.TOP)
+        brightness_label = tkinter.Label(brightness_frame, text = "Brightness settings")
+        brightness_label.pack()
+        brightness_scale = tkinter.Scale(brightness_frame, orient = tkinter.HORIZONTAL, command = self.change_brightness,
+                                         length = self.frame_for_configurations_width, from_ = -255, to = 255, variable = brightness_var)
+        brightness_scale.set(0)
+        brightness_scale.pack(anchor = tkinter.CENTER)
+
+        contrast_var = 1
+        contrast_frame = tkinter.Frame(self.frame_for_configurations, bd = 3, relief = tkinter.SUNKEN)
+        contrast_frame.pack(side = tkinter.TOP)
+        contrast_label = tkinter.Label(contrast_frame, text = "Contrast frame")
+        contrast_label.pack()
+        contrast_scale = tkinter.Scale(contrast_frame, orient = tkinter.HORIZONTAL, command = self.change_contrast,
+                                        from_ = 1, to = 100, variable = contrast_var)
+        contrast_scale.pack(anchor = tkinter.CENTER)
+        
+    def change_contrast(self, var):
+        R, G, B = 0, 1, 2
+        source = self.img.split()
+        contrast_factor = (259 * (int(var) + 255)) / (255 * (259 - int(var)))
+        
+        red = source[R].point(lambda i: 128 + contrast_factor * (i - 128))
+        green = source[G].point(lambda i: 128 + contrast_factor * (i - 128))
+        blue = source[B].point(lambda i: 128 + contrast_factor * (i - 128))
+        
+        source[R].paste(red, None, None)
+        source[G].paste(green, None, None)
+        source[B].paste(blue, None, None)
+        self.image = Image.merge(self.img.mode, source)
+        self.show_image()
     
+    def change_brightness(self, var):
+        R, G, B = 0, 1, 2
+        source = self.img.split()
+
+        red = source[R].point(lambda i: i + int(var))
+        green = source[G].point(lambda i: i + int(var))
+        blue = source[B].point(lambda i: i + int(var))
+        
+        source[R].paste(red, None, None)
+        source[G].paste(green, None, None)
+        source[B].paste(blue, None, None)
+        self.image = Image.merge(self.img.mode, source)
+        self.show_image()
+        
     def open_image(self):
         ##Opening the image. "self.img" will be used as an input for filter methods
         ## and "self.image" will be used to keep an output of filter methods.
@@ -78,6 +134,7 @@ class Filter(tkinter.Frame):
                                         filetypes = (("jpeg files","*.jpg"), ("all files","*.*"))).name)
         if self.img == 0:
             self.choose_filter()
+            self.image_configurations()
 
         self.img = Image.open(self.path_to_image)
         self.image = self.img
@@ -90,7 +147,7 @@ class Filter(tkinter.Frame):
     def save_image_as(self):
         ##Saving an image in the chosen destination and with chosen name.
         path_and_name_to_save = tkinter.filedialog.asksaveasfilename(title = "Save as",
-                                        filetypes = (("jpeg files","*.jpeg"), ("all files","*.*")))
+                                        filetypes = (("jpeg files","*.jpg"), ("all files","*.*")))
         self.image.save(path_and_name_to_save)
 
     def show_image(self):
